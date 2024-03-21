6f4244154abf4de349d6f8aa1eea1b5688c63887
76yoga <brianterer73@gmail.com>
2024-03-21 17:21:37 +0300

    commit this lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+// Write your solution in this fil
+var customerName = 'bob';
+function upperCaseCustomerName () {
+ customerName= customerName.toUpperCase()
+}
+ function setBestCustomer() {
+  bestCustomer ='not bob';
+ }
+
+ function overwriteBestCustomer() {
+    bestCustomer='maybe bob';
+ } 
+ const leastFavoriteCustomer = 'steve';
+// Write a function that attempts to change that constant - notice what JavaScript does when you try to change the constant.
+function changeLeastFavoriteCustomer() {
+  // Attempting to change the constant will result in an error.
+  leastFavoriteCustomer = 'bob'; // This line will throw a TypeError because you cannot reassign a constant.
+}
+
